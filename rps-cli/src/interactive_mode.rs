e4a7ba1f3029909b324cad29b05aa27a40c30b73
David Wesst <david@cocobokostudios.com>
2023-09-05 19:24:13 +0000

    added cli arguments for non-interactive mode

---
+++ rps-cli/src/interactive_mode.rs
@@ -0,0 +1,92 @@
+
+use std::io;
+use rand::{self, Rng};
+
+use rps;
+use rps::choice::{RPSChoice, RPSChoiceError};
+use rps::compare::Compare;
+use rps::result::RPSResult;
+
+pub fn run_interactive_mode() {
+    println!("Let's play Rock, Paper, Scissors!");
+    println!("Best 3 out of 5, okay?");
+
+    let mut quit = false;
+
+    let mut player_wins = 0;
+    let mut cpu_wins = 0;
+
+    'game: loop { // game 
+
+        loop { // round
+            println!("Select (r)ock, (p)aper, or (s)cissors");
+            let cpu_move: RPSChoice = rand::thread_rng().gen();
+
+            let mut player_move = String::new();
+
+            io::stdin()
+                .read_line(&mut player_move) 
+                .expect("Failed to read input");
+
+
+            let player_move : Result<RPSChoice, RPSChoiceError> = player_move.trim().parse();
+            let player_move = match player_move {
+                Ok(player_move_val) => {
+                    // OK
+                    println!("");
+                    println!("You guessed {player_move_val}");
+                    println!("I guessed {cpu_move}");
+                    player_move_val
+                }
+                Err(RPSChoiceError::Unknown(s)) => {
+                    // Err
+                    match &s[..] {
+                        "q" | "quit" => {
+                            println!("Quitting...");
+                            quit = true;
+                            break 'game;
+                        },
+                        _ => {
+                            println!("WTF dude?! {s} is not an option. Try again.");
+                            continue
+                        }
+                    }
+                }
+            };
+
+            let result: RPSResult = player_move.compare(&cpu_move);
+            println!("{}", result);
+
+            match result {
+                RPSResult::Win(_) => {
+                    player_wins += 1;
+                    println!("You win this round.");
+                },
+                RPSResult::Loss(_) => {
+                    cpu_wins += 1;
+                    println!("I win this round");
+                },
+                RPSResult::Tie() => {
+                    println!("Tied round...no points.")
+                }
+            }
+            
+            if player_wins == 3 {
+                println!("\nCongratulations! You won the game.");
+                break;
+            } else if cpu_wins == 3 {
+                println!("\nOh snap! You lost sucka! Better luck next time.");
+                break;
+            } else {
+                println!("You have {player_wins} points and I have {cpu_wins} points.\n");
+                print!("Again. :) ")
+            }
+        }
+        break;
+    }
+
+    if quit == true {
+        println!("Thanks for playing! Hope to see you again soon. 🙂");
+    }
+
+}
--- rps-cli/src/main.rs
+++ rps-cli/src/main.rs
@@ -1,96 +1,21 @@
-//
-// Rock, Paper, Scissors
-//  A game by David Wesst, based on implementation from Riskpeep
-//  Part 1 Reference: https://www.riskpeep.com/2022/07/rock-paper-scissors.html
-//  Part 2 Reference: https://www.riskpeep.com/2022/07/rock-paper-scissors-game-in-rust-ii.html
-use std::io;
-use rand::{self, Rng};
+use clap::Parser;
 
-use rps;
-use rps::choice::{RPSChoice, RPSChoiceError};
-use rps::compare::Compare;
-use rps::result::RPSResult;
+mod interactive_mode;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value_t = false, help = "Run RPS in non-interactive mode.")]
+    non_interactive: bool
+}
 
 fn main() {
-    println!("Let's play Rock, Paper, Scissors!");
-    println!("Best 3 out of 5, okay?");
+    let args: Args = Args::parse();
 
-    let mut quit = false;
-
-    let mut player_wins = 0;
-    let mut cpu_wins = 0;
-
-    'game: loop { // game 
-
-        loop { // round
-            println!("Select (r)ock, (p)aper, or (s)cissors");
-            let cpu_move: RPSChoice = rand::thread_rng().gen();
-
-            let mut player_move = String::new();
-
-            io::stdin()
-                .read_line(&mut player_move) 
-                .expect("Failed to read input");
-
-
-            let player_move : Result<RPSChoice, RPSChoiceError> = player_move.trim().parse();
-            let player_move = match player_move {
-                Ok(player_move_val) => {
-                    // OK
-                    println!("");
-                    println!("You guessed {player_move_val}");
-                    println!("I guessed {cpu_move}");
-                    player_move_val
-                }
-                Err(RPSChoiceError::Unknown(s)) => {
-                    // Err
-                    match &s[..] {
-                        "q" | "quit" => {
-                            println!("Quitting...");
-                            quit = true;
-                            break 'game;
-                        },
-                        _ => {
-                            println!("WTF dude?! {s} is not an option. Try again.");
-                            continue
-                        }
-                    }
-                }
-            };
-
-            let result: RPSResult = player_move.compare(&cpu_move);
-            println!("{}", result);
-
-            match result {
-                RPSResult::Win(_) => {
-                    player_wins += 1;
-                    println!("You win this round.");
-                },
-                RPSResult::Loss(_) => {
-                    cpu_wins += 1;
-                    println!("I win this round");
-                },
-                RPSResult::Tie() => {
-                    println!("Tied round...no points.")
-                }
-            }
-            
-            if player_wins == 3 {
-                println!("\nCongratulations! You won the game.");
-                break;
-            } else if cpu_wins == 3 {
-                println!("\nOh snap! You lost sucka! Better luck next time.");
-                break;
-            } else {
-                println!("You have {player_wins} points and I have {cpu_wins} points.\n");
-                print!("Again. :) ")
-            }
-        }
-        break;
+    if args.non_interactive == true {
+        println!("Non-interactive mode...COMING SOON!");
     }
-
-    if quit == true {
-        println!("Thanks for playing! Hope to see you again soon. 🙂");
+    else {
+        interactive_mode::run_interactive_mode();
     }
-
 }
