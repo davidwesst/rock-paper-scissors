78a4991eeeeafc27d62dce59e63956239ea7799c
David Wesst <david@cocobokostudios.com>
2023-09-17 19:27:59 +0000

    fixed install dependencies script

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash
-export DEBIAN_FRONTEND=noninteractive
-
 sudo apt update
-sudo apt upgrade
-sudo apt install -y g++ pkg-config libx11-dev libasound2-dev libudev-dev libwayland-dev libxkbcommon-dev
+DEBIAN_FRONTEND=noninteractive sudo apt upgrade -y
+DEBIAN_FRONTEND=noninteractive sudo apt install -y g++ pkg-config libx11-dev libasound2-dev libudev-dev libwayland-dev libxkbcommon-dev
